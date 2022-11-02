c20b8b41240bce811d3a68ba0776dc34502b3cfd
Maik Lorenz <lorenz.maik@web.de>
2022-11-02 21:26:24 +0000

    Fix import

--- starter/10-Omnifood-React/src/App.js
+++ starter/10-Omnifood-React/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Header from "./components/Header/Header";
-import Grid from "./components/HowItWorks/HowItWorks";
+import HowItWorks from "./components/HowItWorks/HowItWorks";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <main>
         <Hero />
-        <Grid />
+        <HowItWorks />
       </main>
     </>
   );
